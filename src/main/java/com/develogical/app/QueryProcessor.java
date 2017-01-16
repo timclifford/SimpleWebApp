f460348a1cfa5b6b443323f0abb808aaba8478a9
timclifford <timmarkclifford@gmail.com>
2017-01-16 15:55:11 +0000

    Deliberately broke build

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -2,7 +2,7 @@ package com.develogical.app;
 
 public class QueryProcessor {
     public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare")) {
+        if (query.toLowerCase().contains("thomas")) {
             return "Christopher Shakespeare (1564 - 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
